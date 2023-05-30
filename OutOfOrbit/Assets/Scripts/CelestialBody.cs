9a2bc8e5aa328d805c322a8dd333b9d9ace47ce5
Daniel Heilmann <Daniel.Heilmann@stud.h-da.de>
2023-05-30 17:42:40 +0200

    Attempted to implement planet rotation
    - Deleted RandomMoonGenerator, as it did not add anything useful to the project (at least right now)
    - Added checkerboard texture / Updated material to make planet rotation visible

--- OutOfOrbit/Assets/Scripts/CelestialBody.cs
+++ OutOfOrbit/Assets/Scripts/CelestialBody.cs
@@ -3,18 +3,19 @@ using UnityEngine;
 public class CelestialBody : MonoBehaviour
 {
     public GameObject orbitsAround;
-    public float speed = 1f;
+    public float orbitingSpeed = 1f;
     public float distance = 1f;
     public Quaternion orbitingAngle;
-
+    public float rotationsPerSecond = 1f;
+    public Vector3 inclination;
     private float timeSinceOrbitStart = 0f;
-    private GameObject rotationEmpty = null;
+    private GameObject realOrbitingCenter = null;
 
     public static CelestialBody Construct(GameObject AttachComponentToThis, GameObject orbitsAround, float speed, float distance, Quaternion orbitingAngle)
     {
         CelestialBody component = AttachComponentToThis.AddComponent<CelestialBody>();
         component.orbitsAround = orbitsAround;
-        component.speed = speed;
+        component.orbitingSpeed = speed;
         component.distance = distance;
         component.orbitingAngle = orbitingAngle;
         return component;
@@ -23,25 +24,41 @@ public class CelestialBody : MonoBehaviour
     private void Start()
     {
         if (orbitsAround == null)
+        {
             Debug.LogWarning($"{this.name} does not have an orbiting center.");
+            return;
+        }
 
-        rotationEmpty = new GameObject($"RotationOrigin of {this.name}");
-        rotationEmpty.transform.SetParent(orbitsAround.transform);
-        transform.SetParent(rotationEmpty.transform);
+        realOrbitingCenter = new GameObject($"RotationOrigin of {this.name}");
+        realOrbitingCenter.transform.SetParent(orbitsAround.transform);
+        transform.SetParent(realOrbitingCenter.transform);
     }
 
-    private void Update()
+    private void LateUpdate()
     {
-        rotationEmpty.transform.localRotation = orbitingAngle;
-        this.transform.localRotation = Quaternion.Inverse(orbitingAngle);
+        if (orbitsAround != null)
+            Orbit();
+
+        // RotateAroundSelf();
+    }
+
+    private void Orbit()
+    {
+        realOrbitingCenter.transform.localRotation = orbitingAngle;
+        // this.transform.localRotation = Quaternion.Inverse(orbitingAngle);
 
         ////////////////////////////////
 
-        timeSinceOrbitStart += Time.deltaTime * (speed / distance);
+        timeSinceOrbitStart += Time.deltaTime;
 
         Vector3 newPos;
-        newPos = new Vector3(Mathf.Sin(timeSinceOrbitStart) * distance, 0, Mathf.Cos(timeSinceOrbitStart) * distance);
+        newPos = new Vector3(Mathf.Sin(timeSinceOrbitStart * (orbitingSpeed / distance)) * distance, 0, Mathf.Cos(timeSinceOrbitStart * (orbitingSpeed / distance)) * distance);
 
         transform.localPosition = newPos;
+    }
+
+    private void RotateAroundSelf()
+    {
+        transform.RotateAround(transform.position, transform.up, timeSinceOrbitStart * rotationsPerSecond * 360f);
     }
 }
