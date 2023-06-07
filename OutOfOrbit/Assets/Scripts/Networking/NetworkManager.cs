2b165133091ccf05425104f689738d5685286183
Daniel Heilmann <Daniel.Heilmann@stud.h-da.de>
2023-06-07 12:43:22 +0200

    Set up & Configured Photon Pun and Photon Voice
    - Set up all required GameObjects in scene "MultiplayerTest"
    - Supported features:
       - Player Sync (for VR head and hands)
       - Syncing of XRGrabInteractables (via derived class)
       - Voice Chat
    - Not yet implemented features:
       - VR hand animation sync

---
+++ OutOfOrbit/Assets/Scripts/Networking/NetworkManager.cs
@@ -0,0 +1,36 @@
+using UnityEngine;
+using Photon.Pun;
+using Photon.Realtime;
+
+public class NetworkManager : MonoBehaviourPunCallbacks
+{
+    void Start()
+    {
+        ConnectedToServer();
+    }
+    private void ConnectedToServer()
+    {
+        PhotonNetwork.ConnectUsingSettings();
+        Debug.Log("Try To Connect To Server...");
+    }
+    public override void OnConnectedToMaster()
+    {
+        Debug.Log("Connected To Server.");
+        base.OnConnectedToMaster();
+        RoomOptions roomOptions = new RoomOptions();
+        roomOptions.MaxPlayers = 2;
+        roomOptions.IsVisible = true;
+        roomOptions.IsOpen = true;
+        PhotonNetwork.JoinOrCreateRoom("DefaultRoom", roomOptions, TypedLobby.Default);
+    }
+    public override void OnJoinedRoom()
+    {
+        Debug.Log("Join a room.");
+        base.OnJoinedRoom();
+    }
+    public override void OnPlayerEnteredRoom(Player newPlayer)
+    {
+        Debug.Log("New player joined the room");
+        base.OnPlayerEnteredRoom(newPlayer);
+    }
+}
