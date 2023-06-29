83ffae6434d108a2049fdcd4aefef6d81cf48b78
Daniel Heilmann <Daniel.Heilmann@stud.h-da.de>
2023-06-29 02:01:00 +0200

    Implemented automatic selection of default player based on DisplayCount
    - Depending on the amount of displays connected to the device / computer, this system chooses CAVE or HMD as the default player type, unless manually overwritten by assigning either value in the Settings script

--- OutOfOrbit/Assets/Scripts/XRPlayer/PlayerHandler.cs
+++ OutOfOrbit/Assets/Scripts/XRPlayer/PlayerHandler.cs
@@ -14,11 +14,22 @@ public class PlayerHandler : MonoBehaviour
         else
             DestroyImmediate(this);
 
+        DisplayCountChecker();
         SetCurrentPlayerBasedOnSettings();
     }
 
     private void SetCurrentPlayerBasedOnSettings()
     {
         XRPlayer.SetAsCurrent(Settings.defaultXRPlayer);
+    }
+
+    private void DisplayCountChecker()
+    {
+        int displayCount = Display.displays.Length;
+        Debug.Log($"{displayCount} Display{(displayCount > 1 ? "s" : "")} connected: Trying to set default player to {(displayCount >= 7 ? "CAVE" : "HMD")}.");
+        if (displayCount >= 7)
+            Settings.defaultXRPlayer = PlayerType.CAVE;
+        else
+            Settings.defaultXRPlayer = PlayerType.HMD;
     }
 }
