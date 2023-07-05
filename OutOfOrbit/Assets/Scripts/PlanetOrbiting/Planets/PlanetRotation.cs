8d225d8563356c3c8544c3ad3182994b95e74781
Daniel Heilmann <Daniel.Heilmann@stud.h-da.de>
2023-07-05 22:04:54 +0200

    Improved PlanetRotation script
    - Now rotates GameObject OnValidate() based on variable rotationAxisTilt
    - Axis line visualization is drawn in OnDrawGizmos() now

--- OutOfOrbit/Assets/Scripts/PlanetOrbiting/Planets/PlanetRotation.cs
+++ OutOfOrbit/Assets/Scripts/PlanetOrbiting/Planets/PlanetRotation.cs
@@ -2,6 +2,7 @@ using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
 
+[ExecuteInEditMode]
 public class PlanetRotation : MonoBehaviour
 {
     public Vector3 rotationAxisTilt;
@@ -19,7 +20,18 @@ public class PlanetRotation : MonoBehaviour
 
     private void Rotate()
     {
-        Debug.DrawLine(transform.position + transform.up, transform.position - transform.up, Color.white, Universe.physicsTimeStep);
         transform.RotateAround(transform.position, transform.up, Universe.physicsTimeStep * rotationsPerSecond * 360f);
+    }
+
+    private void OnValidate()
+    {
+        transform.localRotation = Quaternion.Euler(rotationAxisTilt);
+    }
+
+    private void OnDrawGizmos()
+    {
+        Vector3 lineLength = transform.up * transform.localScale.magnitude * 0.5f;
+        Gizmos.color = Color.white;
+        Gizmos.DrawLine(transform.position + lineLength, transform.position - lineLength);
     }
 }
